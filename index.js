62196947933de9940c00232bd778fa3478521b2d
Mitchell <Hmitchell.hall@gmail.com>
2021-08-31 14:06:25 -0500

    done

--- index.js
+++ index.js
@@ -21,3 +21,62 @@ const allWagesFor = function () {
     return payable
 }
 
+
+let employeeRecord;
+
+function createEmployeeRecord(testEmployee){
+  let employeeRecord = {
+        firstName: testEmployee[0],
+        familyName: testEmployee[1],
+        title: testEmployee[2],
+        payPerHour: testEmployee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord
+}
+createEmployeeRecord(testEmployee)
+
+function createEmployeeRecords(employeeArr){
+   return  employeeArr.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateStamp){
+     this.timeInEvents.push({
+            type: 'TimeIn',
+            hour: parseInt(dateStamp.slice(-4)),
+            date: dateStamp.slice(0,10)
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+     this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.slice(-4)),
+        date: dateStamp.slice(0,10)
+    })
+    return this
+}
+
+function hoursWorkedOnDate(formDate){
+    let timeOut = this.timeOutEvents.find(element => element.date === formDate)
+    let timeIn = this.timeInEvents.find(element => element.date === formDate)
+   return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date){
+ return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, name){
+   return srcArray.find(element => element.firstName === name)
+}
+
+function calculatePayroll(array){
+    let total = 0
+    for(let obj of array){
+      total +=  allWagesFor.call(obj)
+   }
+   return total
+}
